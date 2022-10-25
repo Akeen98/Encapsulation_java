7c590b15ba0ba8964a9b28c08507e54ccb9dfe05
Akeen Bnadara <akeenbandara9@gmail.com>
2022-10-25 23:05:28 +0530

    create Enacpsualtion in java

---
+++ Encapsulation2.java
@@ -0,0 +1,9 @@
+public class Encapsulation2{
+    public static void main(String[] args){
+        Encapsulation obj = new Encapsulation();
+        obj.setName("akeen");
+        System.out.println(obj.getName());
+        obj.setAge(50);
+        System.out.println(obj.getAge());
+    }
+}
